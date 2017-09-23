47be31ac1e3f1002ac5449287f80e2e75f52b1a4
Anamaria Todor <ana.todor@webyxdesign.com>
2017-09-23 15:56:58 +0200

    Implemented tileLocation

--- src/main/scala/observatory/Interaction.scala
+++ src/main/scala/observatory/Interaction.scala
@@ -1,6 +1,7 @@
 package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
+import scala.math._
 
 /**
   * 3rd milestone: interactive visualization
@@ -9,20 +10,24 @@ object Interaction {
 
   /**
     * @param zoom Zoom level
-    * @param x X coordinate
-    * @param y Y coordinate
+    * @param x    X coordinate
+    * @param y    Y coordinate
     * @return The latitude and longitude of the top-left corner of the tile, as per http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
     */
   def tileLocation(zoom: Int, x: Int, y: Int): Location = {
-    ???
+    val n = pow(2.0, zoom)
+    val lon_deg = x / n * 360.0 - 180.0
+    val lat_rad = atan(sinh(Pi * (1 - 2 * y / n)))
+    val lat_deg = toDegrees(lat_rad)
+    Location(lat_deg, lon_deg)
   }
 
   /**
     * @param temperatures Known temperatures
-    * @param colors Color scale
-    * @param zoom Zoom level
-    * @param x X coordinate
-    * @param y Y coordinate
+    * @param colors       Color scale
+    * @param zoom         Zoom level
+    * @param x            X coordinate
+    * @param y            Y coordinate
     * @return A 256×256 image showing the contents of the tile defined by `x`, `y` and `zooms`
     */
   def tile(temperatures: Iterable[(Location, Double)], colors: Iterable[(Double, Color)], zoom: Int, x: Int, y: Int): Image = {
@@ -31,15 +36,16 @@ object Interaction {
 
   /**
     * Generates all the tiles for zoom levels 0 to 3 (included), for all the given years.
-    * @param yearlyData Sequence of (year, data), where `data` is some data associated with
-    *                   `year`. The type of `data` can be anything.
+    *
+    * @param yearlyData    Sequence of (year, data), where `data` is some data associated with
+    *                      `year`. The type of `data` can be anything.
     * @param generateImage Function that generates an image given a year, a zoom level, the x and
     *                      y coordinates of the tile and the data to build the image from
     */
   def generateTiles[Data](
-    yearlyData: Iterable[(Int, Data)],
-    generateImage: (Int, Int, Int, Int, Data) => Unit
-  ): Unit = {
+                           yearlyData: Iterable[(Int, Data)],
+                           generateImage: (Int, Int, Int, Int, Data) => Unit
+                         ): Unit = {
     ???
   }
 
